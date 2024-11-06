fe9dd008a7ebbe04bab1b4e60bb08c4857629592
Rui Cruzeiro <ruicruzeiro@gmail.com>
2024-11-06 01:15:06 +0000

    port setup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import io
 import regex
 from flask import Flask, render_template, request, jsonify
@@ -94,4 +95,5 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
